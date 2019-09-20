b9d8355042119d8e13367446710a653c01f99239
Joel Maatkamp <joel.maatkamp@kentivo.com>
2019-09-20 20:24:52 +0200

    Completed basic setup

---
+++ src/store.js
@@ -0,0 +1,15 @@
+import thunk from 'redux-thunk'
+
+import { routerMiddleware } from "connected-react-router";
+import { composeWithDevTools } from 'redux-devtools-extension/logOnlyInProduction';
+import { applyMiddleware, createStore } from "redux";
+
+import history from "./history";
+import reducer from "./reducers/index";
+
+
+const middleware = composeWithDevTools(applyMiddleware(
+    thunk, routerMiddleware(history)
+));
+
+export default createStore(reducer, {}, middleware);
