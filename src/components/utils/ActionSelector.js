549d91b883f57a5a6f33f39463d9112fc03045e2
Joel Maatkamp <joel.maatkamp@kentivo.com>
2019-11-27 22:50:46 +0100

    Added utils & refactored types

---
+++ src/components/utils/ActionSelector.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import * as PropTypes from 'prop-types';
+
+import {connect} from 'react-redux';
+
+
+class ActionSelector extends React.Component {
+    render() {
+        if (this.props.action === this.props.current || this.props.current === "default" && this.props.default)
+            return this.props.children;
+
+        return <React.Fragment />
+    }
+}
+
+ActionSelector.propTypes = {
+    action: PropTypes.string.isRequired,
+    default: PropTypes.bool,
+    current: PropTypes.string.isRequired,
+};
+
+ActionSelector.defaultProps = {
+    default: false,
+};
+
+const mapStateToProps = state => ({
+    current: state.page.action,
+});
+
+const mapDispatchToProps = {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ActionSelector);
